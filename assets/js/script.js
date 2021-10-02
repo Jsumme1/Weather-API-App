e5f18b1303e7f13de6863ec0e89c6d97c588b5b8
Jsummers1 <julie.summers2000@gmail.com>
2021-10-02 10:53:59 -0500

    API calls working

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,19 +10,39 @@ const currentTempEl = document.getElementById("current-temp");
 
 $("#search-btn").click(function (event) {
     event.preventDefault();
-    var citySearch = $("#text-input").val();
+    var citySearch = $("#city-input").val();
   console.log(citySearch)
 
-   fetch('https://api.openweathermap.org/data/2.5/forecast?q='+citySearch+'&units=imperial&appid=b21816d3467ea13d650a6252dc04b76a'
+ fetch('https://api.openweathermap.org/data/2.5/forecast?q='+citySearch+'&units=imperial&appid=b21816d3467ea13d650a6252dc04b76a'
      )
        .then(function (response) {
         return response.json();
        })
       .then(function (data) {
-        console.log(data);
-        console.log(data.list[0].dt_txt);
-        console.log(data.list[0].main.temp);
-        var dateTitle=$("#date");
-        dateTitle.txt(data.list[0].dt_txt);
+        console.log(data.city.coord.lat);
+        console.log(data.city.coord.lon);
+        var latitude = data.city.coord.lat;
+        var longitude = data.city.coord.lon;
+
+            
+       return fetch(
+        'https://api.openweathermap.org/data/2.5/onecall?lat='+latitude+'&lon='+longitude+'&exclude=minutely,hourly,alerts&units=imperial&appid=b21816d3467ea13d650a6252dc04b76a'
+       )
+         .then(function (response) {
+           return response.json();
+         })
+         .then(function (data2) {
+           console.log(data2);
+           console.log(data.list[0].dt_txt);
+           console.log(data.list[0].main.temp);
+           var dateTitle = $("#date");
+           dateTitle.txt(data.list[0].dt_txt);
+         });
+
+
      });
-});
+
+    });
+ 
+
+
