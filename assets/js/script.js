a3b438d3afb70e237e400c5bf640bd8072933603
Jsummers1 <julie.summers2000@gmail.com>
2021-10-03 13:32:02 -0500

    added more variables

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,29 +1,32 @@
 "use strict";
 //List of constants:
 const topCityNameEl = document.getElementById("top-city");
+const topCityIconEl = document.getElementById("current-icon");
 const topCityDateEl = document.getElementById("current-date");
 const topCityTempEl = document.getElementById("current-temp");
 const topCityWindEl = document.getElementById("current-wind");
 const topCityHumidityEl = document.getElementById("current-humidity");
 const topCityUviEl = document.getElementById("current-uvi");
+const newCitiesEl = document.getElementsByClassName("new-cities");
+
+const dayOneDate = document.getElementById("date-day1");
+const iconDayOne = document.getElementById("icon-day1");
+const tempDayOne = document.getElementById("temp-day1");
+const windDayOne= document.getElementById("wind-day1");
+const humdityDayOne = document.getElementById("humidity-day1");
 
 
-const dateEl = document.getElementById("date");
-const currentWeatherItems = document.getElementById("current-weather-items");
-const form = document.querySelector(".form");
-const weatherForecastEl = document.getElementById("weather-forecast");
-const currentTempEl = document.getElementById("current-temp");
 
-//function to append html document with current city weather info, add button
-function displayCurrentInfo() {
-  topCityTempEl.textContent = currentInfo.currentTemp;
-
-  // add button to append to button list
-};
 
 // function to pull data for 5/4 day forecast
 
-function displayFourDays (){};
+// function displayFourDays (){
+//   dayOneDate.textContent = dayOneDate;
+//   iconDayOne.textContent = dayOneIcon;
+//   tempDayOne.textContent = dayOneTemp;
+//   humdityDayOne.textContent = dayOneHumidity;
+//   windDayOne.textContent = dayOneWindSpeed;
+// };
 
 $("#search-btn").click(function (event) {
     event.preventDefault();
@@ -55,32 +58,50 @@ $("#search-btn").click(function (event) {
            console.log(data2.current.humidity);
            console.log(data2.current.uvi);
 
-          const currentInfo = {
-            // turn dt to current date - unix time to normal people time
-            todaysDate: new Date(data2.current.dt * 1000).toLocaleDateString(
-              "en-Us"
-            ),
-            currentTemp: data2.current.temp,
-            currentWindSpeed: data2.current.wind_speed,
-            currentHumidity: data2.current.humidity,
-            uvi: data2.current.uvi,
-           
-           
-            //  save dayOne data (tomorrow) constant
-            dayOneDate: new Date(data2.daily[1].dt * 1000).toLocaleDateString(
-              "en-Us"
-            ),
-            dayOneTemp: data2.daily[1].temp.day,
-            dayOneWindSpeed: data2.daily[1].wind_speed,
-            dayOneHumidity: data2.daily[1].humidity,
-            dayOneIcon: data2.daily[1].weather[0].icon,
-          }; 
+           const currentInfo = {
+             // turn dt to current date - unix time to normal people time
+             todaysDate: new Date(data2.current.dt * 1000).toLocaleDateString(
+               "en-Us"
+             ),
+             currentIcon: data2.current.weather[0].icon,
+             currentTemp: data2.current.temp,
+             currentWindSpeed: data2.current.wind_speed,
+             currentHumidity: data2.current.humidity,
+             uvi: data2.current.uvi,
+             currentCity: citySearch,
+           };
 
-          //  call function to display/append current data
-          displayCurrentWeather(currentInfo);
-        
-          // for loop to call/display/append 4/5 day forecast
-           
+           //function to append html document with current city weather info, add button
+           function displayCurrentWeather(currentInfo) {
+             topCityTempEl.textContent = currentTemp;
+             topCityNameEl.textContent = currentCity;
+             topCityNameEl.textContent = currentIcon;
+             topCityDateEl.textContent = todaysDate;
+             topCityWindEl.textContent = currentWindSpeed;
+             topCityHumidityEl.textContent = CurrentHumidity;
+             topCityUviEl.textContent = uvi;
+
+             // add button to append to button list
+             var btn = document.createElement("BUTTON");
+             btn.innerHTML = "citySearch";
+             document.newCitiesEl.appendChild(btn);
+           }
+
+           //  call function to display/append current data
+          //  displayCurrentWeather();
+
+           //  save dayOne data (tomorrow) constant
+           const DailyInfo = {
+             dayOneDate: new Date(data2.daily[1].dt * 1000).toLocaleDateString(
+               "en-Us"
+             ),
+             dayOneTemp: data2.daily[1].temp.day,
+             dayOneWindSpeed: data2.daily[1].wind_speed,
+             dayOneHumidity: data2.daily[1].humidity,
+             dayOneIcon: data2.daily[1].weather[0].icon,
+           };
+
+           // for loop to call/display/append 4/5 day forecast
          });
 
 
